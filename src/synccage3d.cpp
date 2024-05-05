7264c379ca9236fbe151e13cfc2973956101e120
Feiyue Zhang <feiyue_zhang@brown.edu>
2024-05-04 23:20:06 -0400

    Fixed multi-drag in side-by-side

--- src/synccage3d.cpp
+++ src/synccage3d.cpp
@@ -62,9 +62,32 @@ void SyncCage3D::moveAllAnchors(int vertex, Vector3f pos)
 
     m_isSynced = true;
     if (!m_linkedCage->isSynced()) {
-        m_linkedCage->moveAllAnchors(vertex, pos);
+        m_linkedCage->moveAllAnchors(vertex, pos, anchors);
     }
     m_isSynced = false;
+}
+
+void SyncCage3D::moveAllAnchors(int vertex, Vector3f pos, const std::unordered_set<int>& anchors)
+{
+    std::vector<Eigen::Vector3f> new_vertices = m_shape_cage.getVertices();
+
+    Vector3f delta = pos - new_vertices[vertex];
+
+    // Apply delta to all anchors
+    for (int a : anchors) {
+        new_vertices[a] += delta;
+    }
+
+    // Update cage vertex positions
+    updateCage(new_vertices, vertex, pos);
+    heMesh.updateVertexPos(new_vertices);
+
+    // Update object vertex positions
+    object3D.updateVertices(heMesh);
+    std::vector<Eigen::Vector3f> new_object_vertices = object3D.getVertices();
+
+    m_shape_cage.setVertices(new_vertices);
+    m_shape_object.setVertices(new_object_vertices);
 }
 
 bool SyncCage3D::isSynced()
